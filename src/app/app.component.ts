8690cb9435a5b027f65c10fbf7d92e034430b108
godfrey-tech <godfreymasha13@gmail.com>
2024-02-25 14:21:26 +0200

    Fix: Invoice download and tested invoice with multiple pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,16 @@ export class AppComponent {
   @ViewChild('content', {static: false}) el!: ElementRef;
   
   title = 'Invoice';
-
-  makePDF(){
-
-    let pdf = new jsPDF('p','px','A4');
-
-    pdf.html(this.el.nativeElement,{
-      callback:(pdf)=>{
-        pdf.save("TaxInvoice.pdf"); 
-      }
+  makePDF() {
+    // Use html2canvas to capture the entire content as an image
+    html2canvas(this.el.nativeElement).then(canvas => {
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jsPDF('p', 'mm', 'A4');
+      const imgProps = pdf.getImageProperties(contentDataURL);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save('TaxInvoice.pdf');
     });
   }
 }
